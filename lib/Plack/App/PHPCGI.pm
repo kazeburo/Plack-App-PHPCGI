debcaa4a8dc7b36629039d72d9f2b0e41e2386a3
Masahiro Nagano <kazeburo@gmail.com>
2012-12-27 00:26:30 +0900

    initial commit

---
+++ lib/Plack/App/PHPCGI.pm
@@ -0,0 +1,32 @@
+package Plack::App::PHPCGI;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::App::PHPCGI -
+
+=head1 SYNOPSIS
+
+  use Plack::App::PHPCGI;
+
+=head1 DESCRIPTION
+
+Plack::App::PHPCGI is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
